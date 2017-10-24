cb48e02466d897f1c2df5cab8aa4ca74b831f779
giuliom95 <giuliomartella1995@gmail.com>
2017-10-24 13:22:11 +0200

    Version 0.2.2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
+const Verts = Array{Float64, 2}
+const Cells = SparseMatrixCSC{Int8, Int}
+const Cell = SparseVector{Int8, Int}
+
 include("./planar_arrangement.jl")
+
 include("./dimension_travel.jl")
 include("./utilities.jl")
