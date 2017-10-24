55526015c0622dbf49185fd44bc697587a0fa141
Francesco Furiani <francesco@furio.me>
2017-10-24 14:32:43 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,6 @@ const Verts = Array{Float64, 2}
 const Cells = SparseMatrixCSC{Int8, Int}
 const Cell = SparseVector{Int8, Int}
 
-try
-   Pkg.installed("TRIANGLE")
-catch
-   Pkg.clone("https://github.com/furio/TRIANGLE.jl.git")
-   Pkg.build("TRIANGLE")
-end
-
 include("./planar_arrangement.jl")
 include("./dimension_travel.jl")
 include("./utilities.jl")
