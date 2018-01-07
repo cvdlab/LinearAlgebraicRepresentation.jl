b2c1beea27c0db45813e93c73ee0807912ec86f5
apaoluzzi <apaoluzzi@mac.com>
2018-01-07 18:55:59 +0100

    Version 0.3.7

--- test/larlib.jl
+++ test/larlib.jl
@@ -27,4 +27,32 @@
       @test typeof(LARLIB.uboundary2(FV,EV))==SparseMatrixCSC{Int8,Int64}
       @test size(LARLIB.uboundary2(FV,EV))==(length(FV),length(EV)) 
    end
+
+   @testset "boundary2 Tests" begin
+      V,cells = LARLIB.larCuboids([2,1,1],true) 
+      VV,EV,FV,CV = cells
+      @test typeof(LARLIB.boundary2(FV,EV))==SparseMatrixCSC{Int8,Int64}
+      @test size(LARLIB.boundary2(FV,EV))==(length(FV),length(EV)) 
+      @test sum(findnz(LARLIB.boundary2(FV,EV))[3])==0
+   end
+
+   @testset "chaincomplex Tests" begin
+      V,cells = LARLIB.larCuboids([2,1,1],true) 
+      VV,EV,FV,CV = cells
+      bases,coboundaries = LARLIB.chaincomplex(V,FV,EV)[2:3]
+      @test typeof(bases)==Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1},
+         Array{Array{Int64,1},1}}
+      @test typeof(coboundaries)==Tuple{SparseMatrixCSC{Int8,Int64},
+         SparseMatrixCSC{Int8,Int64},SparseMatrixCSC{Int8,Int64}}
+      @testset "$basis" for basis in bases
+         @test typeof(basis)==Array{Array{Int64,1},1}
+      end
+      @testset "$coboundary" for coboundary in coboundaries
+         @test typeof(coboundary)==SparseMatrixCSC{Int8,Int64}
+      end
+   end
+   
+   @testset "collection2model Tests" begin
+      @test typeof(collection)==Array{Array{Array,1},1}
+   end
 end
