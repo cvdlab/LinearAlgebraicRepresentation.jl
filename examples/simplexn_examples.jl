3a634d5d007e5a4348c7c692ee0acafa7aa19aa5
apaoluzzi <apaoluzzi@mac.com>
2018-07-31 12:51:26 +0200

    crumbs

--- examples/simplexn_examples.jl
+++ examples/simplexn_examples.jl
@@ -1,4 +1,6 @@
+using LARLIB
 
+using LARVIEW
 
 const VOID = [[]], [[1]]
 
@@ -9,40 +11,40 @@ V = [[0,0] [1,0] [2,0] [0,1] [1,1] [2,1] [0,2] [1,2] [2,2]];
 FV = [[1,2,4],[2,3,5],[3,5,6],[4,5,7],[5,7,8],[6,8,9]];
 pattern = repeat([1,2,-3],outer=4);
 model = (V,FV);
-W,FW = extrudeSimplicial(model, pattern);
+W,FW = LARLIB.extrudeSimplicial(model, pattern);
 LARVIEW.view(W,FW)
 
 # example 2
 
-model = extrudeSimplicial( VOID, ones(10) )
+model = LARLIB.extrudeSimplicial( VOID, ones(10) )
 LARVIEW.view(model)
-model = extrudeSimplicial( model, ones(10) )
+model = LARLIB.extrudeSimplicial( model, ones(10) )
 LARVIEW.view(model)
-model = extrudeSimplicial( model, ones(10) )
+model = LARLIB.extrudeSimplicial( model, ones(10) )
 LARVIEW.view(model)
 
 
 # example 3
 
-model = extrudeSimplicial( VOID, repeat([1,-1],outer=10) )
+model = LARLIB.extrudeSimplicial( VOID, repeat([1,-1],outer=10) )
 LARVIEW.view(model)
-model = extrudeSimplicial( model, repeat([1,-1],outer=10) )
+model = LARLIB.extrudeSimplicial( model, repeat([1,-1],outer=10) )
 LARVIEW.view(model)
 
 
 # example 4
 
-grid_2d = simplexGrid([3,3])
+grid_2d = LARLIB.simplexGrid([3,3])
 LARVIEW.view(grid_2d)
-grid_3d = simplexGrid([2,3,4])
+grid_3d = LARLIB.simplexGrid([2,3,4])
 LARVIEW.view(grid_3d)
-V,CV = simplexGrid([1,1,1])
+V,CV = LARLIB.simplexGrid([1,1,1])
 LARVIEW.view(V,CV)
 
 # example 5
 
-SK2 = (simplexFacets(CV))
+SK2 = LARLIB.simplexFacets(CV)
 LARVIEW.view(V, SK2)
-SK1 = simplexFacets(SK2[2])
+SK1 = LARLIB.simplexFacets(SK2[2])
 LARVIEW.view(V, SK1)
 
