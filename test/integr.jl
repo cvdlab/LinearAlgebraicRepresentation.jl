c108745d0547caec5ff7a5c4928c0f07e532441a
apaoluzzi <apaoluzzi@mac.com>
2018-08-01 15:49:06 +0200

    fixed integration tests and internal docs

--- test/integr.jl
+++ test/integr.jl
@@ -6,31 +6,31 @@ using Base.Test
 @testset "Integration Tests" begin
 
 	@testset "M" begin
-		@test M(0,0)==0.5
-		@test M(1,0)==0.16666666666666666
-		@test M(2,0)==0.08333333333333333
-		@test M(3,0)==0.05
-		@test M(1,1)==0.041666666666666685
-		@test M(2,0)==0.08333333333333333
-		@test M(2,1)==0.016666666666666663
-		@test M(3,0)==0.05
-		@test M(3,1)==0.008333333333333338
-		@test M(3,2)==0.0023809523809523586
-		@test M(3,3)==0.0008928571428571397
+		@test LARLIB.M(0,0)==0.5
+		@test LARLIB.M(1,0)==0.16666666666666666
+		@test LARLIB.M(2,0)==0.08333333333333333
+		@test LARLIB.M(3,0)==0.05
+		@test LARLIB.M(1,1)==0.041666666666666685
+		@test LARLIB.M(2,0)==0.08333333333333333
+		@test LARLIB.M(2,1)==0.016666666666666663
+		@test LARLIB.M(3,0)==0.05
+		@test LARLIB.M(3,1)==0.008333333333333338
+		@test LARLIB.M(3,2)==0.0023809523809523586
+		@test LARLIB.M(3,3)==0.0008928571428571397
 	end
 
 
 	@testset "TT" begin
 		tau=[0.0 1.0 0.0; 0.0 0.0 1.0; 0.0 0.0 0.0];
-		@test TT(tau, 0,0,0)==0.5
-		@test TT(tau, 1,0,0)==0.16666666666666666
-		@test TT(tau, 1,1,0)==0.041666666666666685
-		@test TT(tau, 1,1,1)==0.0
-		@test TT(tau, 2,0,0)==0.08333333333333333
-		@test TT(tau, 2,1,0)==0.016666666666666663
-		@test TT(tau, 2,2,0)==0.005555555555555545
-		@test TT(tau, 2,2,1)==0.0
-		@test TT(tau, 2,2,2)==0.0
+		@test LARLIB.TT(tau, 0,0,0)==0.5
+		@test LARLIB.TT(tau, 1,0,0)==0.16666666666666666
+		@test LARLIB.TT(tau, 1,1,0)==0.041666666666666685
+		@test LARLIB.TT(tau, 1,1,1)==0.0
+		@test LARLIB.TT(tau, 2,0,0)==0.08333333333333333
+		@test LARLIB.TT(tau, 2,1,0)==0.016666666666666663
+		@test LARLIB.TT(tau, 2,2,0)==0.005555555555555545
+		@test LARLIB.TT(tau, 2,2,1)==0.0
+		@test LARLIB.TT(tau, 2,2,2)==0.0
 	end
 
 
@@ -38,11 +38,11 @@ using Base.Test
 		V = [0.0 1.0 0.0; 0.0 0.0 1.0; 0.0 0.0 0.0];
 		FV = [[1,2,3]];
 		P = V,FV;
-		@test II(P, 0,0,0)==0.5
-		@test II(P, 1,0,0)==0.16666666666666666
-		@test II(P, 0,1,0)>=0.1666666666666666
-		@test II(P, 0,0,1)==0.0
-		@test II(P, 1,1,1)==0.0
+		@test LARLIB.II(P, 0,0,0)==0.5
+		@test LARLIB.II(P, 1,0,0)==0.16666666666666666
+		@test LARLIB.II(P, 0,1,0)>=0.1666666666666666
+		@test LARLIB.II(P, 0,0,1)==0.0
+		@test LARLIB.II(P, 1,1,1)==0.0
 	end
 
 
@@ -50,27 +50,27 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test III(P, 0,0,0)>0.166666666
-		@test III(P, 0,0,0)<0.166666888
-		@test III(P, 1,0,0)>0.041666666
-		@test III(P, 1,0,0)<0.041666888
-		@test III(P, 0,1,0)>0.041666666
-		@test III(P, 0,1,0)<0.041666888
-		@test III(P, 0,0,1)>0.041666666
-		@test III(P, 0,0,1)<0.041666888
-		@test III(P, 10,10,10)>1.3377e-11
-		@test III(P, 10,10,10)<1.3388e-11
+		@test LARLIB.III(P, 0,0,0)>0.166666666
+		@test LARLIB.III(P, 0,0,0)<0.166666888
+		@test LARLIB.III(P, 1,0,0)>0.041666666
+		@test LARLIB.III(P, 1,0,0)<0.041666888
+		@test LARLIB.III(P, 0,1,0)>0.041666666
+		@test LARLIB.III(P, 0,1,0)<0.041666888
+		@test LARLIB.III(P, 0,0,1)>0.041666666
+		@test LARLIB.III(P, 0,0,1)<0.041666888
+		@test LARLIB.III(P, 10,10,10)>1.3377e-11
+		@test LARLIB.III(P, 10,10,10)<1.3388e-11
 	end
 
 
 	@testset "surface" begin
 		V,FV = LARLIB.simplexGrid([1,1]);
 		P = [V;[0 0 0 0]], FV
-		@test surface(P)==1.0
+		@test LARLIB.surface(P)==1.0
 		p = LARLIB.Struct([LARLIB.t(0.5,0.5,0), LARLIB.r(0,0,pi/4), P]);
 		q = LARLIB.struct2lar(p);
-		@test surface(q)>1.0000000
-		@test surface(q)<1.0000222
+		@test LARLIB.surface(q)>1.0000000
+		@test LARLIB.surface(q)<1.0000222
 	end
 
 
@@ -78,8 +78,8 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test volume(P)>0.166666666
-		@test volume(P)<0.166668888
+		@test LARLIB.volume(P)>0.166666666
+		@test LARLIB.volume(P)<0.166668888
 	end
 
 
@@ -87,18 +87,18 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test firstMoment(P)[1]<0.0416667
-		@test firstMoment(P)[1]>0.0416665
+		@test LARLIB.firstMoment(P)[1]<0.0416667
+		@test LARLIB.firstMoment(P)[1]>0.0416665
 
-		@test firstMoment(P)[2]<0.0416667
-		@test firstMoment(P)[2]>0.0416665
+		@test LARLIB.firstMoment(P)[2]<0.0416667
+		@test LARLIB.firstMoment(P)[2]>0.0416665
 
-		@test firstMoment(P)[3]<0.0416667
-		@test firstMoment(P)[3]>0.0416665
+		@test LARLIB.firstMoment(P)[3]<0.0416667
+		@test LARLIB.firstMoment(P)[3]>0.0416665
 
-		@test abs(firstMoment(P)[1]-firstMoment(P)[2])<0.00001
-		@test abs(firstMoment(P)[2]-firstMoment(P)[3])<0.00001
-		@test abs(firstMoment(P)[3]-firstMoment(P)[1])<0.00001
+		@test abs(LARLIB.firstMoment(P)[1]-LARLIB.firstMoment(P)[2])<0.00001
+		@test abs(LARLIB.firstMoment(P)[2]-LARLIB.firstMoment(P)[3])<0.00001
+		@test abs(LARLIB.firstMoment(P)[3]-LARLIB.firstMoment(P)[1])<0.00001
 	end
 
 
@@ -106,18 +106,18 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test secondMoment(P)[1]<0.0166666669
-		@test secondMoment(P)[1]>0.0166666664
+		@test LARLIB.secondMoment(P)[1]<0.0166666669
+		@test LARLIB.secondMoment(P)[1]>0.0166666664
 
-		@test secondMoment(P)[2]<0.0166666669
-		@test secondMoment(P)[2]>0.0166666664
+		@test LARLIB.secondMoment(P)[2]<0.0166666669
+		@test LARLIB.secondMoment(P)[2]>0.0166666664
 
-		@test secondMoment(P)[3]<0.0166666669
-		@test secondMoment(P)[3]>0.0166666664
+		@test LARLIB.secondMoment(P)[3]<0.0166666669
+		@test LARLIB.secondMoment(P)[3]>0.0166666664
 
-		@test abs(secondMoment(P)[1]-secondMoment(P)[2])<0.00001
-		@test abs(secondMoment(P)[2]-secondMoment(P)[3])<0.00001
-		@test abs(secondMoment(P)[3]-secondMoment(P)[1])<0.00001
+		@test abs(LARLIB.secondMoment(P)[1]-LARLIB.secondMoment(P)[2])<0.00001
+		@test abs(LARLIB.secondMoment(P)[2]-LARLIB.secondMoment(P)[3])<0.00001
+		@test abs(LARLIB.secondMoment(P)[3]-LARLIB.secondMoment(P)[1])<0.00001
 	end
 
 
@@ -125,14 +125,14 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test inertiaProduct(P)[1]<0.00833666
-		@test inertiaProduct(P)[1]>0.00833000
+		@test LARLIB.inertiaProduct(P)[1]<0.00833666
+		@test LARLIB.inertiaProduct(P)[1]>0.00833000
 
-		@test inertiaProduct(P)[2]<0.00833666
-		@test inertiaProduct(P)[2]>0.00833000
+		@test LARLIB.inertiaProduct(P)[2]<0.00833666
+		@test LARLIB.inertiaProduct(P)[2]>0.00833000
 
-		@test inertiaProduct(P)[3]<0.00833666
-		@test inertiaProduct(P)[3]>0.00833000
+		@test LARLIB.inertiaProduct(P)[3]<0.00833666
+		@test LARLIB.inertiaProduct(P)[3]>0.00833000
 	end
 
 
@@ -140,14 +140,14 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test centroid(P)[1]<0.26
-		@test centroid(P)[1]>0.24
+		@test LARLIB.centroid(P)[1]<0.26
+		@test LARLIB.centroid(P)[1]>0.24
 
-		@test centroid(P)[2]<0.26
-		@test centroid(P)[2]>0.24
+		@test LARLIB.centroid(P)[2]<0.26
+		@test LARLIB.centroid(P)[2]>0.24
 
-		@test centroid(P)[3]<0.26
-		@test centroid(P)[3]>0.24
+		@test LARLIB.centroid(P)[3]<0.26
+		@test LARLIB.centroid(P)[3]>0.24
 	end
 
 
@@ -155,14 +155,14 @@ using Base.Test
 		V = [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0];
 		FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]];
 		P = V,FV;
-		@test inertiaMoment(P)[1]<0.0333555
-		@test inertiaMoment(P)[1]>0.0333111
+		@test LARLIB.inertiaMoment(P)[1]<0.0333555
+		@test LARLIB.inertiaMoment(P)[1]>0.0333111
 
-		@test inertiaMoment(P)[2]<0.0333555
-		@test inertiaMoment(P)[2]>0.0333111
+		@test LARLIB.inertiaMoment(P)[2]<0.0333555
+		@test LARLIB.inertiaMoment(P)[2]>0.0333111
 
-		@test inertiaMoment(P)[3]<0.0333555
-		@test inertiaMoment(P)[3]>0.0333111
+		@test LARLIB.inertiaMoment(P)[3]<0.0333555
+		@test LARLIB.inertiaMoment(P)[3]>0.0333111
 	end
 
 end
