bc130c5162efb49661dd530455e4ba8933206661
Francesco Furiani <francesco@furio.me>
2018-08-12 00:03:35 +0200

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,1 @@
-try
-    Pkg.installed("Triangle")
-catch
-    Pkg.clone("https://github.com/cvdlab/Triangle.jl.git")
-    Pkg.checkout("Triangle", "ver-0.1.0") # Julia 0.6
-    Pkg.build("Triangle")
-end
+
