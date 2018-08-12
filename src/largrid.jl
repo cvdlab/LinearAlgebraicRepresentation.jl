b5fc78e5f7655d9e0596a7ea247fc68e0fdc0c99
furio <francesco@furio.me>
2018-08-12 21:51:29 +0200

    broadcast vector

--- src/largrid.jl
+++ src/largrid.jl
@@ -233,9 +233,9 @@ function larCellProd(cellLists::Array{Cells,1})::Cells
    subscripts = cart([collect(range(0, length=shape)) for shape in shapes])
    indices = [collect(tuple) for tuple in subscripts]
    jointCells = [cart([cells[k] for (k,cells) in zip(index,cellLists)]) 
-   				for index in indices+1]
+                    for index in indices .+ 1]
    convertIt = index2addr([ (length(cellLists[k][1]) > 1) ? shape+1 : shape 
-      for (k,shape) in enumerate(shapes) ])     
+                    for (k,shape) in enumerate(shapes) ])
    [vcat(map(convertIt, map(collect,jointCells[j]))...) for j in 1:length(jointCells)]
 end
 
