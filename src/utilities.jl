0d8a32c45d5e8634e31f49ba7c0b897fc282f4e8
apaoluzzi <apaoluzzi@mac.com>
2018-08-30 16:44:54 +0200

    added lar2obj2D and obj2lar2D

--- src/utilities.jl
+++ src/utilities.jl
@@ -688,3 +688,122 @@ function space_arrangement(V::Points, EV::ChainOp, FE::ChainOp, multiproc::Bool=
     return rV, rEV, rFE, rCF
 end
 
+using DataStructures
+using LinearAlgebraicRepresentation
+using LARVIEW
+Lar = LinearAlgebraicRepresentation
+View = LARVIEW.view
+
+
+###  2D triangulation
+##################################################################
+""" 
+	obj2lar2D(path::AbstractString)::LinearAlgebraicRepresentation.LARmodel
+
+Read a *triangulation* from file, given its `path`. Return a `LARmodel` object
+"""
+function obj2lar2D(path::AbstractString)::LinearAlgebraicRepresentation.LARmodel
+    vs = Array{Float64, 2}(0, 3)
+    edges = Array{Array{Int, 1}, 1}()
+    faces = Array{Array{Int, 1}, 1}()
+
+    open(path, "r") do fd
+		for line in eachline(fd)
+			elems = split(line)
+			if length(elems) > 0
+				if elems[1] == "v"
+					x = parse(Float64, elems[2])
+					y = parse(Float64, elems[3])
+					z = parse(Float64, elems[4])
+					vs = [vs; x y z]
+				elseif elems[1] == "f"
+					# Ignore the vertex tangents and normals
+					_,v1,v2,v3 = map(parse, elems)
+					append!(edges, map(sort,[[v1,v2],[v2,v3],[v3,v1]]))
+					push!(faces, [v1, v2, v3])
+				end
+				edges = collect(Set(edges))
+			end
+		end
+	end
+    return (vs, [edges,faces])::LinearAlgebraicRepresentation.LARmodel
+end
+
+
+""" 
+	lar2obj2D(V::LinearAlgebraicRepresentation.Points, 
+			cc::LinearAlgebraicRepresentation.ChainComplex)::String
+
+Produce a *triangulation* from a `LARmodel`. Return a `String` object
+"""
+function lar2obj2D(V::LinearAlgebraicRepresentation.Points, cc::LinearAlgebraicRepresentation.ChainComplex)::String
+    assert(length(cc) == 2)
+    copEV, copFE = cc
+    V = [V zeros(size(V, 1))]
+
+    obj = ""
+    for v in 1:size(V, 1)
+        obj = string(obj, "v ", 
+        	round(V[v, 1], 6), " ", 
+        	round(V[v, 2], 6), " ", 
+        	round(V[v, 3], 6), "\n")
+    end
+
+    print("Triangulating")
+    triangulated_faces = triangulate2D(V, cc[1:2])
+    println("DONE")
+
+	obj = string(obj, "\n")
+	for f in 1:copFE.m
+		triangles = triangulated_faces[f]
+		for tri in triangles
+			t = tri
+			#t = copCF[c, f] > 0 ? tri : tri[end:-1:1]
+			obj = string(obj, "f ", t[1], " ", t[2], " ", t[3], "\n")
+		end
+	end
+
+    return obj
+end
+
+
+""" 
+	triangulate2D(V::LinearAlgebraicRepresentation.Points, 
+			cc::LinearAlgebraicRepresentation.ChainComplex)::Array{Any, 1}
+
+Compute a *CDT* for each face of a `ChainComplex`. Return an `Array` of triangles.
+"""
+function triangulate2D(V::LinearAlgebraicRepresentation.Points, cc::LinearAlgebraicRepresentation.ChainComplex)::Array{Any, 1}
+    copEV, copFE = cc
+    triangulated_faces = Array{Any, 1}(copFE.m)
+	
+    for f in 1:copFE.m       
+        edges_idxs = copFE[f, :].nzind
+        edge_num = length(edges_idxs)
+        edges = Array{Int64,1}[] #zeros(Int64, edge_num, 2)
+
+        fv = LinearAlgebraicRepresentation.buildFV(copEV, copFE[f, :])
+        vs = V[fv, :]
+        
+        for i in 1:length(fv)
+        	edge = Int64[0,0]
+            edge[1] = fv[i]
+            edge[2] = i == length(fv) ? fv[1] : fv[i+1]
+            push!(edges,edge::Array{Int64,1})
+        end
+        edges = hcat(edges...)'
+        
+        triangulated_faces[f] = Triangle.constrained_triangulation(
+        vs, fv, edges, fill(true, edge_num))
+        tV = V[:, 1:2]
+        
+        area = LinearAlgebraicRepresentation.face_area(tV, copEV, copFE[f, :])
+        if area < 0 
+            for i in 1:length(triangulated_faces[f])
+                triangulated_faces[f][i] = triangulated_faces[f][i][end:-1:1]
+            end
+        end
+    end
+
+    return triangulated_faces
+end
