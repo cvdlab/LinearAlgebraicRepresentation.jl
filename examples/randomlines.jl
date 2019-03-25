0b59dad5795e9faf7cd24d983bae48250ee9a77e
apaoluzzi <apaoluzzi@mac.com>
2019-03-25 07:50:04 +0100

    crumbs

--- examples/randomlines.jl
+++ examples/randomlines.jl
@@ -2,7 +2,7 @@ using LinearAlgebraicRepresentation
 Lar = LinearAlgebraicRepresentation
 using Plasm
 
-n = 50 #20000
+n = 1000 #20000
 t = 0.4 #0.15
 V = zeros(Float64,2,2*n)
 EV = [zeros(Int64,2) for k=1:n]
@@ -22,6 +22,9 @@ model = (V,EV)
 Sigma = Lar.spaceindex(model)
 
 
-Plasm.view(V,EV)
+#Plasm.view(V,EV)
+model = V,EV
+W,EW = Lar.fragmentlines(model)
+Plasm.viewexploded(W,EW)(1.2,1.2,1.2)
 
 
