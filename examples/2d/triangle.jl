a5698e9f108d55d4540d855b500d64f5cfef872e
apaoluzzi <apaoluzzi@mac.com>
2019-05-14 09:19:57 +0200

    added triangulate2d() to Lar

--- examples/2d/triangle.jl
+++ examples/2d/triangle.jl
@@ -5,3 +5,11 @@ triangle = (V,EV,FV)
 model = Lar.Struct([ triangle, Lar.t(.15,.15), Lar.s(.5,.5), triangle ])
 V,EV,FV = Lar.struct2lar(model)
 Plasm.view(Plasm.numbering(0.5)((V,[[[k] for k=1:size(V,2)], EV])))
+
+triangles = Lar.triangulate2d(V, EV)
+Plasm.view(V,triangles)
+
+ev = map(sort,cat([[[u,v], [v,w], [w,u]] for (u,v,w) in triangles]))
+# remove duplicated edges from triangulation
+ev_nodups = collect(Set(ev))
+Plasm.view(Plasm.numbering(0.35)((V,[VV, ev_nodups, triangles])))
