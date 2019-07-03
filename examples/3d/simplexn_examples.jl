d50fa2a26af7383999e0dc8f919d3e3f781e908f
apaoluzzi <apaoluzzi@mac.com>
2019-07-03 05:52:00 +0200

    other examples converted

--- examples/3d/simplexn_examples.jl
+++ examples/3d/simplexn_examples.jl
@@ -1,6 +1,8 @@
 using LinearAlgebraicRepresentation
 Lar = LinearAlgebraicRepresentation
-using Plasm
+using ViewerGL
+GL = ViewerGL
+
 
 VOID = [[]], [[1]]
 
@@ -12,39 +14,41 @@ FV = [[1,2,4],[2,3,5],[3,5,6],[4,5,7],[5,7,8],[6,8,9]];
 pattern = repeat([1,2,-3],outer=4);
 model = (V,FV);
 W,FW = Lar.extrudeSimplicial(model, pattern);
-Plasm.view(W,FW)
+GL.VIEW([ GL.GLGrid(W,FW, GL.COLORS[7], 0.5) ]);
 
 # example 2
 
 model = Lar.extrudeSimplicial( VOID, ones(10) )
-Plasm.view(model)
-model = Lar.extrudeSimplicial( model, ones(10) )
-Plasm.view(model)
-model = Lar.extrudeSimplicial( model, ones(10) )
-Plasm.view(model)
+GL.VIEW([ GL.GLGrid(model..., GL.COLORS[7], 0.5) ]); #TODO: check
 
+model = Lar.extrudeSimplicial( model, ones(10) )
+GL.VIEW([ GL.GLLines(model..., GL.COLORS[7]),
+        GL.GLAxis(GL.Point3d(0,0,0),GL.Point3d(1,1,1)) ]);
+
+model = Lar.extrudeSimplicial( model, ones(10) )
+GL.VIEW([ GL.GLGrid(model..., GL.COLORS[7], 0.5) ]);
 
 # example 3
 
 model = Lar.extrudeSimplicial( VOID, repeat([1,-1],outer=10) )
-Plasm.view(model)
+GL.VIEW([ GL.GLGrid(model..., GL.COLORS[7], 0.5) ]); #TODO: check
+
 model = Lar.extrudeSimplicial( model, repeat([1,-1],outer=10) )
-Plasm.view(model)
+GL.VIEW([ GL.GLGrid(model..., GL.COLORS[7],0.7),
+        GL.GLAxis(GL.Point3d(0,0,0),GL.Point3d(1,1,1)) ]);
 
 
 # example 4
 
 grid_2d = Lar.simplexGrid([3,3])
-Plasm.view(grid_2d)
+GL.VIEW([ GL.GLGrid(grid_2d..., GL.COLORS[7],0.7),
+        GL.GLLines(grid_2d...),    # TODO:  check lines
+        GL.GLAxis(GL.Point3d(0,0,0),GL.Point3d(1,1,1)) ]);
+
 grid_3d = Lar.simplexGrid([2,3,4])
-Plasm.view(grid_3d)
+GL.VIEW([ GL.GLGrid(grid_3d..., GL.COLORS[7]),
+        GL.GLAxis(GL.Point3d(0,0,0),GL.Point3d(1,1,1)) ]);  # TODO  check
+
 V,CV = Lar.simplexGrid([1,1,1])
-Plasm.view(V,CV)
-
-# example 5
-
-SK2 = Lar.simplexFacets(CV)
-Plasm.view(V, SK2)
-SK1 = Lar.simplexFacets(SK2)
-Plasm.view(V, SK1)
-
+GL.VIEW([ GL.GLGrid(V,CV, GL.COLORS[7]),
+        GL.GLAxis(GL.Point3d(0,0,0),GL.Point3d(1,1,1)) ]);  # TODO  check
