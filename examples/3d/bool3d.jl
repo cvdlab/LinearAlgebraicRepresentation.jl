3bb755ec6b29d5e81fd619762b5879f307923292
apaoluzzi <apaoluzzi@mac.com>
2019-09-18 05:35:33 +0200

    fixed examples/3d/bool3d.jl

--- examples/3d/bool3d.jl
+++ examples/3d/bool3d.jl
@@ -1,6 +1,6 @@
 using LinearAlgebraicRepresentation, ViewerGL, SparseArrays
 Lar = LinearAlgebraicRepresentation; GL = ViewerGL
-using Base.union
+import Base.union
 
 # 3D Boolean example generation
 #-------------------------------------------------------------------------------
@@ -17,11 +17,11 @@ assembly = Lar.Struct([ cube,
     Lar.t(.3,.4,.25), Lar.r(pi/5,0,0), Lar.r(0,0,pi/12), cube,
     Lar.t(-.2,.4,-.2), Lar.r(0,pi/5,0), Lar.r(0,pi/12,0), cube ])
 
-V,FV,EV = Lar.struct2lar(assembly)
-GL.VIEW([ GL.GLGrid(V,FV), GL.GLFrame ]);
+    V,FV,EV = Lar.struct2lar(assembly)
+    GL.VIEW([ GL.GLGrid(V,FV), GL.GLFrame ]);
 
-W, (copEV, copFE, copCF), boolmatrix = Lar.bool3d(assembly)
-Matrix(boolmatrix)
+    W, (copEV, copFE, copCF), boolmatrix = Lar.bool3d(assembly)
+    Matrix(boolmatrix)
 #three-chains = [ for k = 1:3]
 
 A = boolmatrix[:,2]
@@ -29,17 +29,17 @@ B = boolmatrix[:,3]
 C = boolmatrix[:,4]
 AorB = A .| B
 AandB = A .& B
-AxorB = AorB .& (.! AandB)
+AxorB = AorB .& (.!AandB) # = A .⊻ B
 AorBorC = A .| B .| C
 AorBorC = .|(A, B, C)
 AandBandC = A .& B .& C
 AandBandC = .&(A, B, C)
-AminusBminusC = .&(A, .!B, .!C) # A - B - C
+AminBminC = .&(A, .!B, .!C) # A - B - C
 
-unione = .|(A,B,C)
+unione = .|(A, B, C)
 unione = Matrix(copCF)' * Int.(unione) # coord vector of Faces
 intersection = Matrix(copCF)' * Int.(AandBandC) # coord vector of Faces
-difference = Matrix(copCF)' * Int.(AminusBminusC) # coord vector of Faces
+difference = Matrix(copCF)' * Int.(AminBminC) # coord vector of Faces
 
 V,CVs,FVs,EVs = Lar.pols2tria(W, copEV, copFE, copCF) # whole assembly
 Fs = unione
@@ -50,7 +50,7 @@ V,CVs,FVs,EVs = Lar.pols2tria(W, copEV, copFE, copCF, Fs) # part of assembly
 
 
 # EV = Lar.cop2lar(copEV)
-# EVor = [ev for (k,ev) in enumerate(EV) if abs(union[k])==1 ]
+# EVor = [ev for (k,ev) in enumerate(EV) if abs(unione[k])==1 ]
 # EVand = [ev for (k,ev) in enumerate(EV) if abs(intersection[k])==1 ]
 # EVxor = [ev for (k,ev) in enumerate(EV) if abs(xor[k])==1 ]
 
@@ -58,11 +58,6 @@ V,CVs,FVs,EVs = Lar.pols2tria(W, copEV, copFE, copCF, Fs) # part of assembly
 
 
 GL.VIEW(GL.GLExplode(V,FVs,1.,1.,1.,99,1));
-GL.VIEW(GL.GLExplode(V,EVs,1.,1.,1.,99,1));
+GL.VIEW(GL.GLExplode(V,EVs,1.,1.,1.,1,1));
 meshes = GL.GLExplode(V,CVs[2:end],1.5,1.5,1.5,99,1);
 GL.VIEW( push!( meshes, GL.GLFrame) );
-
-# GL.VIEW(GL.GLExplode(V,[EVor],1.,1.,1.,99,1));
-
-
-GL.VIEW([ GL.GLGrid(V,EV, GL.COLORS[1],0.5), GL.GLFrame ]);
