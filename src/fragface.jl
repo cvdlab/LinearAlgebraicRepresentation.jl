aedc92f1ea60fc1d72ee43b1e4e8e5b3cc3f00a1
apaoluzzi <apaoluzzi@mac.com>
2020-01-15 14:25:31 +0100

    restored 0<α<1 && 0<β<1

--- src/fragface.jl
+++ src/fragface.jl
@@ -131,7 +131,6 @@ Intersect all (non-simmetric) pairs of `line` in `linestore`.
 function computeparams(linestore,linenum)
 	m = length(linestore)
 	params = [[] for i=1:m]
-	err = 1.0e-6
 	for h=1:(m-1)
 		(x1,y1,x2,y2) = reshape(linestore[h][1:2,:],1,4)
 		for k=(h+1):m
@@ -157,7 +156,7 @@ function computeparams(linestore,linenum)
 					 (β,α) = 999
 				end
 			end
-			if -err<α<1+err && -err<β<1+err
+			if 0<α<1 && 0<β<1
 				push!(params[h], α)
 				push!(params[k], β)
 			end
