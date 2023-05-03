51af227631de7dba6c2ad00a267cff4c29f45ca0
apaoluzzi <apaoluzzi@mac.com>
2023-05-03 16:29:17 +0200

    fixed localcongruence.jl, and partially build_copCF()

--- src/localcongruence.jl
+++ src/localcongruence.jl
@@ -5,11 +5,19 @@ function chaincongruence(W, Delta_0, Delta_1)
     W = convert(Matrix, W')
     V, vclasses = vcongruence(W)
     EV, eclasses = cellcongruence(Delta_0, vclasses, dim=1)
-    FE, fclasses = cellcongruence(Delta_1, eclasses, dim=2)
     copEW = Lar.lar2cop(convert(Vector{Vector{Int64}}, EV))
+
+    FE, fclasses = cellcongruence(Delta_1, eclasses, dim=2)
     copFE = Lar.lar2cop(convert(Vector{Vector{Int64}}, FE))
+    
+    copFV = convert(Lar.ChainOp, copFE * copEW .÷ 2)
+    FV = convert(Lar.Cells, Lar.cop2lar(copFV))
+    EV = convert(Lar.Cells, EV)
+    copFE = Lar.build_copFE(FV::Lar.Cells, EV::Lar.Cells)
+    copEV = Lar.coboundary_0(EV::Lar.Cells)
+    
     V = convert(Matrix, V')
-    return V, copEW, copFE
+    return V, copEV, copFE
 end
 
 function cellcongruence(Delta, inclasses; dim)
@@ -39,7 +47,7 @@ function vcongruence(V::Matrix; epsilon=1e-6)
     end
    #W = hcat([sum(V[:,class], dims=2)/length(class) for class in vclasses]...)
 @show vclasses
-   sort!(vclasses)
+   sort!(map(sort!,vclasses))
 @show vclasses
    W = hcat([V[:,class[1]] for class in vclasses]...)
    return W, vclasses
